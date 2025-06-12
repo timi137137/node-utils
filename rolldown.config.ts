4cc896bb064d9d8c3783bd8fdd0fb7d82e2dac17
悠静萝莉 <i@mikuhl.cn>
2025-06-13 00:08:26 +0800

    feat: 打包打包

---
+++ rolldown.config.ts
@@ -0,0 +1,48 @@
+import typescript from '@rollup/plugin-typescript';
+import { glob } from 'glob';
+import { defineConfig } from 'rolldown';
+import { dts } from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
+
+const srcEntries = glob.sync('src/**/*.ts', {
+  ignore: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
+});
+
+export default defineConfig([
+  {
+    input: srcEntries,
+    output: [
+      {
+        format: 'es',
+        dir: 'dist/esm',
+        preserveModules: true,
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        format: 'cjs',
+        dir: 'dist/cjs',
+        preserveModules: true,
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript(),
+      terser({
+        format: { comments: false },
+      }),
+    ],
+  },
+  {
+    input: srcEntries,
+    output: [
+      {
+        dir: 'dist/types',
+        format: 'es',
+        preserveModules: true,
+      },
+    ],
+    plugins: [dts()],
+  },
+]);

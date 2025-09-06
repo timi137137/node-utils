a0433ba167f5c1be903b392c8f3d751ca8ced706
悠静萝莉 <i@mikuhl.cn>
2025-09-07 05:36:26 +0800

    feat: 新增相关类型

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export * from './nest';
 export * from './snowflake';
+export * from './utils';
+
+export const DEFAULT_SKIP = 0;
+export const DEFAULT_PAGE = 1;
+export const DEFAULT_LIMIT = 10;
--- src/nest/index.ts
+++ src/nest/index.ts
@@ -1,7 +1,10 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiBody, ApiConsumes, ApiOperation, ApiProperty } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiOperation, ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 import type { Constructor } from '@/utils';
+
+// ==================== Swagger 语法糖 ====================
 
 /**
  * Api文件上传装饰器
@@ -30,6 +33,42 @@ export function ApiFile(fileName: string = 'file'): MethodDecorator {
  * @param summary
  */
 export const ApiSummary = (summary: string): MethodDecorator => ApiOperation({ summary });
+
+// ==================== 分页相关类型 ====================
+
+/**
+ * 分页返回类型
+ */
+export interface Paged<T> {
+  count: number;
+  data: T[];
+}
+
+let DefaultPageLimit = 10;
+const MAX_PAGE = 500;
+
+export function setDefaultPageLimit(limit: number): void {
+  DefaultPageLimit = limit;
+}
+export function getDefaultPageLimit(): number {
+  return DefaultPageLimit;
+}
+
+/**
+ * 分页DTO
+ */
+export class PagedDto {
+  @ApiPropertyOptional() @IsOptional() @IsInt() @Min(1) page: number = 1;
+  @ApiPropertyOptional() @IsOptional() @IsInt() @Min(1) @Max(MAX_PAGE) limit: number = getDefaultPageLimit();
+
+  get skip(): number {
+    return (this.page - 1) * this.limit;
+  }
+
+  set skip(value: number) {
+    // noop
+  }
+}
 
 /**
  * 分页返回值DTO
